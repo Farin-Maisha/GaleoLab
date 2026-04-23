df570d664f1aa88103d0ff4c6a093d92b225ad94
Farzana <farzana.cse.20230104032@aust.edu>
2026-04-23 14:58:52 +0600

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,24 @@
-import { Routes, Route } from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import BaseLayout from './components/layout/BaseLayout'
 import Home from './pages/Home'
 import AboutUs from './pages/AboutUs'
 import Services from './pages/Services'
 import NotFound from './pages/NotFound'
 
-export default function App() {
+function App() {
   return (
-    <Routes>
-      <Route element={<BaseLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<AboutUs />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<BaseLayout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<AboutUs />} />
+          <Route path="services" element={<Services />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
+
+export default App
