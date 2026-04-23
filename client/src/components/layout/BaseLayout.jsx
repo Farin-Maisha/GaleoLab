78e6ab4df24084dc50c9b33c6f2a7122c4930138
Farzana <farzana.cse.20230104032@aust.edu>
2026-04-23 15:00:07 +0600

    base layout

--- client/src/components/layout/BaseLayout.jsx
+++ client/src/components/layout/BaseLayout.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import styles from './BaseLayout.module.css'
 
-export default function BaseLayout() {
+function BaseLayout() {
+  const { pathname } = useLocation()
+
+  // Scroll to top on route change
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }, [pathname])
+
   return (
-    <div className={styles.wrapper}>
+    <div className={styles.layout}>
       <Navbar />
       <main className={styles.main}>
         <Outlet />
@@ -14,3 +22,5 @@ export default function BaseLayout() {
     </div>
   )
 }
+
+export default BaseLayout
